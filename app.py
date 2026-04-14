04fc143fb18c5c215d573ef7b47dd55eceb2055c
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-04-14 11:22:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import string
 from nltk.corpus import stopwords
-import nltk
+import nltk   
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
